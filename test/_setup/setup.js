492973ab52bc6df0be38b6530011f483d1a5d397
Braian <braian.rosas@monks.com>
2025-10-28 17:12:58 -0300

    chore: update packages to a modern version

--- test/_setup/setup.js
+++ test/_setup/setup.js
@@ -2,4 +2,12 @@ require("@babel/register")({
   // Setting this will remove the currently hooked extensions of .es6, `.es`, `.jsx`
   // and .js so you'll have to add them back if you want them to be used again.
   extensions: [".es6", ".es", ".jsx", ".js", ".ts"],
+  presets: [
+    ["@babel/preset-env", {
+      "targets": {
+        "node": "14"
+      }
+    }],
+    "@babel/preset-typescript"
+  ]
 });
